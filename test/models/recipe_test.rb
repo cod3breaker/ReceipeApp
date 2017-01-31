556c2fc026b34617834088061fe0e2f064b4401e
Ravindra Herath <ravisherath@gmail.com>
2017-01-31 15:40:20 +1100

    testing the recipe model for name presence

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  def setup
+    @recipe = Recipe.new(name: "Chicken Fried Rice", summary: "This is a dumb Chicken fried rice recipe",
+                         description: "Add all the ingredients and cook with oil")
+ end
+
+ test "recipe should be valid" do
+   assert @recipe.valid?
+ end
+
+ test "name should be valid" do
+   @recipe.name = ""
+   assert_not @recipe.valid?
+ end
+end

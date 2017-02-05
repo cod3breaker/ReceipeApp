a204b3011b36ef9d2c41237717b45931e4dbdbc3
Ravindra Herath <ravisherath@gmail.com>
2017-02-05 21:45:22 +1100

    adding saving fuctionality to the creating a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,5 +10,21 @@ class RecipesController < ApplicationController
     def new
       @recipe = Recipe.new
     end  
+
+    def create
+      @recipe = Recipe.new(recipe_params)
+      @recipe.chef = Chef.find(1) # hardcode chef until authorization created.
+      if @recipe.save
+        flash[:success] = "Your recipe was created successfully!"
+        redirect_to recipes_path
+      else
+        render :new
+      end
+    end
+
+    private
+        def recipe_params
+            params.require(:recipe).permit(:name, :summary, :description)
+        end
 end
 

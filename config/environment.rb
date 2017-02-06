2809a3a96333edf47781bbb6793d59269edec54a
Ravindra Herath <ravisherath@gmail.com>
2017-02-06 14:32:34 +1100

    implementing the image uploader code

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# following line is the fix for the error Recipe.all in Rails Console
+require 'carrierwave/orm/activerecord'
